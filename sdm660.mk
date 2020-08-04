7e66c9d4fcf0c1ca8e6a09ea89f0abdeb6cb7b6b
Harshal Ahire <hahire@codeaurora.org>
2020-08-05 02:48:23 +0530

    sdm660-common: enable sound trigger hidl hal 2.3
    Change-Id: Ieac067b937c06f1ce4f74abf0b884fc1bf92b5c9

--- sdm660.mk
+++ sdm660.mk
@@ -65,7 +65,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
-    android.hardware.soundtrigger@2.2-impl:32 \
+    android.hardware.soundtrigger@2.3-impl:32 \
     audio.primary.sdm660 \
     audio.r_submix.default \
     audio.usb.default \

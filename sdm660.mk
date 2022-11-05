c3858522d7984d586d81fe3535836be540c8861c
kyasu <kyasu.dev@gmail.com>
2022-11-05 19:31:29 +0900

    sdm660-common: Update to drm HAL 1.2

--- sdm660.mk
+++ sdm660.mk
@@ -169,7 +169,7 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.1.vendor \
+    android.hardware.drm@1.2.vendor \
     android.hardware.drm-service.clearkey
 
 # FM

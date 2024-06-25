9a344e9f4853c3f8df48008d0526746cfa96c578
kyasu <kyasu.dev@gmail.com>
2024-06-25 21:55:24 +0900

    sdm660-common: Migrate light HAL from HIDL to AIDL
    Change-Id: Id80f2d7f96ff25308f57f261ceaf8ba78da800c0

--- sdm660.mk
+++ sdm660.mk
@@ -283,7 +283,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.xiaomi_sdm660
+    android.hardware.light-service.xiaomi_sdm660
 
 # Lineage Health
 PRODUCT_PACKAGES += \

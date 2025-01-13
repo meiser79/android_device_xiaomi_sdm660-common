8a2d6d15d65c24a708ac2ba43592a43f1b5b1041
Michael Bestas <mkbestas@lineageos.org>
2025-01-13 11:03:38 +0200

    sdm660-common: Migrate mount point creation out of Android.mk
    Change-Id: Id5986b8a740e45f864eecdd2bd82d9455f128d6b

--- sdm660.mk
+++ sdm660.mk
@@ -316,6 +316,12 @@ PRODUCT_PACKAGES += \
     libOmxVenc \
     libstagefrighthw
 
+# Partitions
+PRODUCT_PACKAGES += \
+    vendor_bt_firmware_mountpoint \
+    vendor_dsp_mountpoint \
+    vendor_firmware_mnt_mountpoint
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2.vendor \
